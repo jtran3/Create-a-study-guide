cd846cd936305e1785c5f360d94f3075a388f49d
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:41:33 -0500

    Update RunningTest.java

--- RunningTest.java
+++ RunningTest.java
@@ -7,11 +7,12 @@ public class RunningTest {
         ArrayList<Questions> q = new ArrayList<Questions>();
         ArrayList<answers> a = new ArrayList<answers>();
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Do you want to create some questions");
+        while (){
             System.out.println("Please enter a question here ");
                name.add(sc.next());
            System.out.println("Please enter an answer for the question ");
-            name.add(sc.next());
-
+                name.add(sc.next());
+        }
   }
 }
