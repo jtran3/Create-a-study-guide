93b31d8789a2c71461e05f516340cc4f3333af49
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:36:20 -0500

    Create RunningTest.java

---
+++ RunningTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RunningTest {
+
+    public static void main(String[] args) {
+        ArrayList<Questions> q = new ArrayList<Questions>();
+        ArrayList<answers> a = new ArrayList<answers>();
+        Scanner sc = new Scanner(System.in);
+        
+            System.out.println("Please enter a question here ");
+               name.add(sc.next());
+           System.out.println("Please enter an answer for the question ");
+            name.add(sc.next());
+
+  }
+}
