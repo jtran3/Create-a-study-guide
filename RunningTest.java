b952e57b1233d31caf6038a7b4f9243b93d59cc7
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:57:13 -0500

    Update RunningTest.java

--- RunningTest.java
+++ RunningTest.java
@@ -7,12 +7,16 @@ public class RunningTest {
         ArrayList<Questions> q = new ArrayList<Questions>();
         ArrayList<answers> a = new ArrayList<answers>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Do you want to create some questions");
-        while (){
+        System.out.println("Do you want to create some questions (yes or no)");
+        if(sc = "yes"){
             System.out.println("Please enter a question here ");
                name.add(sc.next());
            System.out.println("Please enter an answer for the question ");
                 name.add(sc.next());
+        }
+        else
+        {
+            system.out.println("app will close");
         }
   }
 }
