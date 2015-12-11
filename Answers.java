bab162144b1b4540a4f0c1de210cfc39c1394b2a
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:26:57 -0500

    Update Answers.java

--- Answers.java
+++ Answers.java
@@ -1,28 +1,21 @@
 public class answers
 {
-	//instance variable
 	private String answers;
 
-
-	//no argument constructor
 	public answers()
 	{
 		answers = "";
-
 	}
 	
-	//argument constructor
 	public answers(int aID, String answers)
 	{
 		this.answers = answers;
-
 	}
-
+	
 	public String getAnswers()
 	{
 		return answers;
 	}
-
 
 	public void setAnswers(String answers)
 	{
