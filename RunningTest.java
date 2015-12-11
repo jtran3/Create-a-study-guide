c45659e8c936341b8b09bbd288d438ed7a11ea01
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:57:50 -0500

    Update RunningTest.java

--- RunningTest.java
+++ RunningTest.java
@@ -6,13 +6,13 @@ public class RunningTest {
     public static void main(String[] args) {
         ArrayList<Questions> q = new ArrayList<Questions>();
         ArrayList<answers> a = new ArrayList<answers>();
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Do you want to create some questions (yes or no)");
-        if(sc = "yes"){
+        if(in = "yes"){
             System.out.println("Please enter a question here ");
-               name.add(sc.next());
+               name.add(in.next());
            System.out.println("Please enter an answer for the question ");
-                name.add(sc.next());
+                name.add(in.next());
         }
         else
         {
