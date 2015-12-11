c2fe821ad5fa344e125bef31d85d4c541ff8337f
jtran3 <jtran9808@yahoo.com>
2015-12-11 00:26:09 -0500

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,40 +1,23 @@
 public class Questions
 {
-	//instance variable
 	private String questions;
 	
-	//no argument constructor
 	public Questions()
 	{
 		questions = "";
 	}
-	//argument constructor
 	public Questions(String questions)
 	{
 		this.questions = questions;
 	}
-	
-	
-    
-	/**
-	 * Method name: getQuestions()
-	 * Description: calls the question
-	 * @return the questions
-	 */
+
 	public String getQuestions()
 	{
 		return questions;
 	}
 
-	/**
-	 * Method name: setQuestions
-	 * @param questions
-	 * description: set information of the question
-	 */
 	public void setQuestions(String questions)
 	{
 		this.questions = questions;
 	}
-
-	
 }
